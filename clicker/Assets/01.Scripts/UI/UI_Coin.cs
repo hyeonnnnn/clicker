f9e5fcc9a13cd034bf3faf0aa6b50f577a07db62
huiyeon <128331103+hyeonnnnn@users.noreply.github.com>
2026-01-26 19:37:06 +0900

    feat: 코인 두트윈

--- clicker/Assets/01.Scripts/UI/UI_Coin.cs
+++ clicker/Assets/01.Scripts/UI/UI_Coin.cs
@@ -1,10 +1,13 @@
-﻿using TMPro;
+﻿using DG.Tweening;
+using TMPro;
 using UnityEngine;
 
 public class UI_Coin : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _coinTextUI;
     [SerializeField] private CoinManager _coinManager;
+    [SerializeField] private float _punchScale = 0.2f;
+    [SerializeField] private float _duration = 0.3f;
 
     private void OnEnable()
     {
@@ -19,5 +22,14 @@ public class UI_Coin : MonoBehaviour
     private void UpdateCoinText(int amount)
     {
         _coinTextUI.text = amount.ToString();
+
+        PlayTextEffect();
+    }
+
+    private void PlayTextEffect()
+    {
+        _coinTextUI.transform.DOKill();
+        _coinTextUI.transform.localScale = Vector3.one;
+        _coinTextUI.transform.DOPunchScale(Vector3.one * _punchScale, _duration);
     }
 }
