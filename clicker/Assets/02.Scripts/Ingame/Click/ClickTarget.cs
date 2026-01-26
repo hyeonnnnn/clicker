43bab01a77769ecd5519309e7928123f40a5be26
huiyeon <128331103+hyeonnnnn@users.noreply.github.com>
2026-01-26 14:21:59 +0900

    feat: 타겟 클릭하기

---
+++ clicker/Assets/02.Scripts/Ingame/Click/ClickTarget.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class ClickTarget : MonoBehaviour, Clickable
+{
+    [SerializeField] private string _name;
+
+    public bool OnClick()
+    {
+        Debug.Log($"{_name}: 클릭됨.");
+
+        return true;
+    }
+}
---
+++ clicker/Assets/02.Scripts/Ingame/Click/Clicker.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Clicker : MonoBehaviour
+{
+    public LayerMask ClickLayer;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePosition = Input.mousePosition;
+            Click(mousePosition);
+        }
+    }
+
+    private void Click(Vector2 mousePosition)
+    {
+        Vector2 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        RaycastHit2D hit = Physics2D.Raycast(worldPosition, Vector2.zero);
+        if (hit == true)
+        {
+            Clickable clickable = hit.collider.GetComponent<Clickable>();
+            clickable?.OnClick();
+        }
+    }
+}
