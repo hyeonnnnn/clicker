39e0c6e30b3b7107dcb3d35f85ede5c252c7d366
huiyeon <128331103+hyeonnnnn@users.noreply.github.com>
2026-01-26 15:25:02 +0900

    컬러 플래쉬 피드백

---
+++ clicker/Assets/02.Scripts/Ingame/Feedback/ColorFlashFeedback.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ColorFlashFeedback : MonoBehaviour
+{
+    private Coroutine _coroutine;
+
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+
+    [SerializeField] private Color _originalColor;
+    [SerializeField] private Color _flashColor = Color.white;
+
+    private void Start()
+    {
+        _originalColor = _spriteRenderer.color;
+    }
+
+    public void Play()
+    {
+        if (_coroutine != null)
+        {
+            StopCoroutine(_coroutine);
+            _coroutine = null;
+        }
+
+        _coroutine = StartCoroutine(Play_Coroutine());
+    }
+
+    private IEnumerator Play_Coroutine()
+    {
+        _spriteRenderer.color = _flashColor;
+        yield return new WaitForSeconds(0.3f);
+        _spriteRenderer.color = _originalColor;
+    }
+}
