c6c527ccb1a46455d63b37dd05f00d30df19ee85
huiyeon <128331103+hyeonnnnn@users.noreply.github.com>
2026-01-26 17:21:36 +0900

    feat: shuriken이 행성을 중심으로 공전

---
+++ clicker/Assets/01.Scripts/Ingame/Weapon/Shuriken/ShurikenRotation.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class ShurikenRotation : MonoBehaviour
+{
+    [SerializeField] private float _radius = 2f;
+    [SerializeField] private float _rotationSpeed = 90f;
+
+    private void Start()
+    {
+        ArrangeChildrenInCircle();
+    }
+
+    private void Update()
+    {
+        transform.Rotate(0f, 0f, _rotationSpeed * Time.deltaTime);
+    }
+
+    private void ArrangeChildrenInCircle()
+    {
+        int childCount = transform.childCount;
+        if (childCount == 0) return;
+
+        float angleStep = 360f / childCount;
+
+        for (int i = 0; i < childCount; i++)
+        {
+            float angleDeg = angleStep * i;
+            float angleRad = angleDeg * Mathf.Deg2Rad;
+
+            Vector3 position = new Vector3(
+                Mathf.Cos(angleRad) * _radius,
+                Mathf.Sin(angleRad) * _radius,
+                0f
+            );
+
+            Transform child = transform.GetChild(i);
+            child.localPosition = position;
+            child.localRotation = Quaternion.Euler(0f, 0f, angleDeg);
+        }
+    }
+}
