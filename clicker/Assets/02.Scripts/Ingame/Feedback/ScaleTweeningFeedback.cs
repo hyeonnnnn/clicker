c29425d0a154f4e565b24379021bd08588ae3f22
huiyeon <128331103+hyeonnnnn@users.noreply.github.com>
2026-01-26 16:36:45 +0900

    feat: 행성 회전

--- clicker/Assets/02.Scripts/Ingame/Feedback/ScaleTweeningFeedback.cs
+++ clicker/Assets/02.Scripts/Ingame/Feedback/ScaleTweeningFeedback.cs
@@ -5,6 +5,12 @@ public class ScaleTweeningFeedback : MonoBehaviour, IFeedback
 {
     [SerializeField] private ClickTarget _owner;
 
+    [Header("Punch Settings")]
+    [SerializeField] private float _punchScale = 0.2f;
+    [SerializeField] private float _duration = 0.3f;
+    [SerializeField] private int _vibrato = 6;
+    [SerializeField] private float _elasticity = 0.5f;
+
     private void Awake()
     {
         _owner = GetComponent<ClickTarget>();
@@ -15,9 +21,7 @@ public class ScaleTweeningFeedback : MonoBehaviour, IFeedback
         if (clickInfo.Type == EClickType.Auto) return;
 
         _owner.transform.DOKill();
-        _owner.transform.DOScale(1.2f, 0.6f).OnComplete(() =>
-        {
-            _owner.transform.localScale = Vector3.one;
-        });
+        _owner.transform.localScale = Vector3.one;
+        _owner.transform.DOPunchScale(Vector3.one * _punchScale, _duration, _vibrato, _elasticity);
     }
 }
