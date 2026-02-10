03f43bc44a3a944a2c5e4176acf7a1bd15686760
huiyeon <128331103+hyeonnnnn@users.noreply.github.com>
2026-02-10 17:01:30 +0900

    feat: 인풋 판넬 UI

---
+++ Assets/WebAPITutorial/WebGetCSVTest.cs
@@ -0,0 +1,32 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections.Generic;
+
+public class WebGetCSVTest : MonoBehaviour
+{
+
+    private async void Start()
+    {
+        string result = await GetWebText("https://raw.githubusercontent.com/mongilteacher/skku2_script_study/refs/heads/main/students.csv");
+
+        List<Person> persons = new List<Person>();
+        // 읽어온 CSV 파일을 파싱해서
+        // Person도메인 클래스에 넣고 persons에 추가
+        // List<Person> persons 순회하기
+
+    }
+
+    private async UniTask<string> GetWebText(string url)
+    {
+        var txt = (await UnityWebRequest.Get(url).SendWebRequest()).downloadHandler.text;
+        return txt;
+    }
+}
+
+public struct Person
+{
+    public int Id;
+    public string Name;
+    public int Age;
+}
---
+++ Assets/WebAPITutorial/WebGetImageTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using Cysharp.Threading.Tasks;
+
+public class WebGetImageTest : MonoBehaviour
+{
+    public RawImage MyImage;
+
+    private async void Start()
+    {
+        StartCoroutine(GetTexture());
+
+        MyImage.texture = await GetWebTexture("https://biz.chosun.com/resizer/v2/KIGXZI4TNVDWBIQC3Q7RFLN6JU.png?auth=65113d026a545411c35cf215851720560ea82225a1c1f373383e40f52aaa1f40&width=616");
+    }
+
+    private async UniTask<Texture> GetWebTexture(string url)
+    {
+        var texture = ((DownloadHandlerTexture)(await UnityWebRequestTexture.GetTexture(url).SendWebRequest()).downloadHandler).texture;
+        return texture;
+    }
+
+    IEnumerator GetTexture()
+    {
+        UnityWebRequest www = UnityWebRequestTexture.GetTexture("https://www.bbc.com/korean/articles/cn42dkdq1lwo");
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            Texture myTexture = ((DownloadHandlerTexture)www.downloadHandler).texture;
+        }
+    }
+}
